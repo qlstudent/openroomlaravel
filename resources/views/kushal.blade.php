8115dd973673c861cabe47177e5d4cc0f65c45eb
Kushal <kushaldeveloper@gmail.com>
2018-08-07 17:08:25 -0400

    add a new route to test

---
+++ resources/views/kushal.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>I am a title</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+
+    <div class="content">
+        <div class="title m-b-md">
+            Laravel
+        </div>
+
+        <div class="links">
+            <p>
+                I love lamp.
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/potato', function () {
+    return view('kushal');
+});
